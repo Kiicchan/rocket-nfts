226ee027bec1419214883e0bb495963f8da57c50
Marcos Chacon <marcoschacon39@gmail.com>
2022-08-03 17:37:46 -0300

    absolute path config

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import icon from '@/assets/react.svg'
 function App() {
   return (
     <div className="App center">
       <h1>Rocket NFTs</h1>
+      <img src={icon} alt="react" className='animate-pulse' />
     </div>
   )
 }
