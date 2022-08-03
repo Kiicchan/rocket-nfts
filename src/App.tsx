05f0fc5fa53484b25fcc2eb506e491557c74d84e
Marcos Chacon <marcoschacon39@gmail.com>
2022-08-03 18:03:38 -0300

    add assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import icon from '@/assets/react.svg'
 function App() {
   return (
     <div className="App center">
       <h1>Rocket NFTs</h1>
-      <img src={icon} alt="react" className='animate-pulse' />
     </div>
   )
 }
