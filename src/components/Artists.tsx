b87ba09109dd24a8594a66aabad7bfb0110752a7
Marcos Chacon <marcoschacon39@gmail.com>
2022-08-17 18:37:05 -0300

    Add key prop

--- src/components/Artists.tsx
+++ src/components/Artists.tsx
@@ -14,7 +14,7 @@ export function Artists() {
         <section className="container mx-auto my-36">
             <h2 className="text-5xl font-bold my-36 text-center">Melhores <strong className="text-logo">artistas</strong></h2>
             <div className="flex flex-wrap gap-28 justify-between">
-                {artists.map(artist => <ArtistCard {...artist} />)}
+                {artists.map(artist => <ArtistCard key={artist.name} {...artist} />)}
             </div>
             <div className="center mt-12">
                 <a className="text-xl flex gap-4" href="#">Ver todos os artistas <img src="/assets/icons/arrow-orange.svg" alt="" /></a>
