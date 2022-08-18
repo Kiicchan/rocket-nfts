2be5f9aed5706565b10ae2dcc70c57bc6340b808
Marcos Chacon <marcoschacon39@gmail.com>
2022-08-18 14:15:25 -0300

    Adjust Header colors  and add Header transitions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-const glass = "backdrop-blur border-b !border-white/10 bg-white/5"
+const glass = "backdrop-blur-sm border-b border-white/10 bg-white/5"
 
 export function Header() {
     const [isOnTop, setIsOnTop] = useState(true)
@@ -19,11 +19,11 @@ export function Header() {
     }, [isOnTop])
 
     return (
-        <div className={`fixed top-0 left-0 right-0 z-10 border-[transparent] ${!isOnTop && glass} transition-all`}>
-            <header className="container py-14 flex items-center justify-between">
-                <h1 className="text-4xl font-bold">Rocket <strong className="text-logo">NFTs</strong></h1>
+        <div className={`fixed top-0 left-0 right-0 z-10 ${isOnTop ? 'py-10 border-[transparent]' : glass} transition-all`}>
+            <header className="container py-4 flex items-center justify-between">
+                <h1 className="text-4xl font-bold select-none cursor-default">Rocket <strong className="text-logo">NFTs</strong></h1>
                 <nav>
-                    <ul className="center gap-14 text-lg">
+                    <ul className="center gap-14 text-lg [&>li>a]:p-1 [&>li]:border [&>li]:border-[transparent] [&>li]:rounded [&>li:hover]:border-white">
                         <li>
                             <a href="#">Comprar NFT</a>
                         </li>
@@ -35,7 +35,8 @@ export function Header() {
                         </li>
                     </ul>
                 </nav>
-                <button className="w-56 h-12 bg-white text-black font-bold text-base center gap-4 active:bg-logo active:border-2 hover:opacity-90">
+                <button className="w-56 h-12 bg-white text-black font-bold text-base center gap-4 active:border-2 hover:brightness-90 active:brightness-75 transition-colors"
+                    type={"button"}>
                     <img src="/assets/icons/logo_MetaMask.svg" alt="Fox" />
                     Conectar Carteira
                 </button>
