fbd221bb0807093cc09f6b7975ebb5f32d381513
Marcos Chacon <marcoschacon39@gmail.com>
2022-08-27 01:01:13 -0300

    prerender

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePrerender from 'vite-plugin-prerender'
 import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePrerender({
+      staticDir: path.join(__dirname, 'dist'),
+      routes: ['/'],
+    })
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
