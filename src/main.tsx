f4f1cb74331d35a23d27b55de478c99089873aba
Marcos Chacon <marcoschacon39@gmail.com>
2022-08-27 23:36:43 -0300

    setup hydration

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
+import ReactDOMClient from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const rootElement = document.getElementById("root") as HTMLElement;
+
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(<React.StrictMode><App /></React.StrictMode>, rootElement);
+} else {
+  ReactDOMClient.createRoot(rootElement).render(<React.StrictMode><App /></React.StrictMode>);
+}
